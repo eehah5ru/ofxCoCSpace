3b6224bfc8fc03b2af5436e96ce45a11227c9c0d
eeefff <eeefff.org@gmail.com>
2024-05-06 01:14:04 +0200

    attempt to enable orange pi related gst plugins

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+#include "ofGstVideoPlayer.h"
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <gst/app/gstappsink.h>
+#include "ofConstants.h"
+#include "ofGstUtils.h"
+
+void setupGstPlugins () {
+  GstRegistry *reg = gst_registry_get();
+  GstPluginFeature *plugin;
+  plugin = gst_registry_lookup_feature(reg, "mppvideodec");
+  gst_plugin_feature_set_rank(plugin, GST_RANK_PRIMARY);
+
+}
+
 //========================================================================
 int main( ){
 
+#ifdef LOCAL_CAM
+  LOG_APP_NOTICE() << "using local camera";
+#endif
+
+#ifdef REMOTE_CAM
+  LOG_APP_NOTICE() << "using remote camera";
+#endif
     //Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
     ofEnableGLDebugLog();
     ofGLFWWindowSettings settings;
@@ -11,11 +33,12 @@ int main( ){
     //settings.numSamples = 0; // for RPI performance
     settings.windowMode =  OF_FULLSCREEN; // OF_WINDOW; //can also be OF_FULLSCREEN
     //settings.windowMode =  OF_WINDOW;
-  //  settings.width = GRAB_WIDTH;
+    //  settings.width = GRAB_WIDTH;
 
     auto window = ofCreateWindow(settings);
 
+    setupGstPlugins();
+
     ofRunApp(window, make_shared<ofApp>());
     ofRunMainLoop();
-
 }
